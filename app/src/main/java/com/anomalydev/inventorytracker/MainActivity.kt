7693a8ce9eb2c47701dbcc711f29c3e3e08953ac
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-03 22:37:07 -0800

    Feat: Added a drawer layout
    This drawer layout will be used to allow navigation up.

--- app/src/main/java/com/anomalydev/inventorytracker/MainActivity.kt
+++ app/src/main/java/com/anomalydev/inventorytracker/MainActivity.kt
@@ -2,10 +2,36 @@ package com.anomalydev.inventorytracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.anomalydev.inventorytracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        drawerLayout = binding.drawerLayout
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
 }
